a3d60751845d1ef854667ba5ff0b638a06903aab
colombo0718 <colombo0718@gmail.com>
2019-10-25 17:43:02 +0800

    create operators from cast function

--- COS.js/character.js
+++ COS.js/character.js
@@ -9,8 +9,10 @@ var Character=( function(){
 		var trunk= new THREE.Group();
 		var fs=require('fs') 
 		trunk.name=name
-		trunk.position.set(position.x,position.y,position.z)
+		// trunk.position.set(position.x,position.y,position.z)
+		trunk.position.copy(position)
 		trunk.direction=position.d
+		trunk.position.d=position.d
 		trunk.career=career
 		trunk.control=false	
 		
@@ -18,7 +20,7 @@ var Character=( function(){
 
 		new BVHLoader()
 			.setPath( './actis/' )
-			.load( "male_sway.bvh", function ( result ) {
+			.load( "male_base.bvh", function ( result ) {
 				// ra=result
 				// console.log(result )
 				trunk.skeletonHelper = new THREE.SkeletonHelper( result.skeleton.bones[ 0 ] );
@@ -36,7 +38,7 @@ var Character=( function(){
 				// console.log(trunk.mixer.clipAction( result.clip ))
 				// console.log(trunk.mixer)
 				// trunk.mixer.addEventListener( 'loop', function( e ) {console.log(e.action.paused=true)} )
-				trunk.doing='sway'
+				// trunk.mixer.clipAction( result.clip ).play()
 			} );
 
 		// bones bind trunk -------------------
@@ -55,7 +57,6 @@ var Character=( function(){
 							object.rotateX(Math.PI/2)
 							trunk.getObjectByName('Bone-'+part).add(object);
 						});
-
 				});
 		})
 		// set parameters and Marks
@@ -115,7 +116,7 @@ var Character=( function(){
 		trunk.showMarks=function(){
 			this.markSpace.visible=true
 			this.markSpace.position.copy(this.position)
-
+			// plot blood stick
 			this.markSpace.remove(trunk.markSpace.blood)
 			var geometry = new THREE.BoxGeometry(.05,this.health/10,.05);
 			var bloodcolor=0x0000ff
@@ -127,6 +128,18 @@ var Character=( function(){
 			cube.position.set(-.4,1-this.health/20,.4)
 			this.markSpace.blood=cube
 			this.markSpace.add(cube);
+			//set small flag
+			if(this.markSpace.gflag!=undefined && this.markSpace.gflag!=undefined){
+				// console.log(this.markSpace.gflag.visible)
+				if(this.stamina>0){
+					this.markSpace.gflag.visible=true
+					this.markSpace.rflag.visible=false
+				}else{
+					this.markSpace.gflag.visible=false
+					this.markSpace.rflag.visible=true
+				}
+			}
+			
 
 		}
 
@@ -161,11 +174,14 @@ var Character=( function(){
 						alter.direction=alter.direction%4
 						break;
 				}
+				alter.position.d=alter.direction
 			},delay*1000)
 		}
 
 		// actions ------------
 		var actionNames=['base','sway','walk','hack','sits','tlef','trig','swep','spur','salute']
+
+		trunk.defaultAction="base"
 
 		trunk.actions=[]
 		actionNames.forEach(function(name){
@@ -174,6 +190,10 @@ var Character=( function(){
 				.load( 'male_'+name+'.bvh', function ( result ) {
 					trunk.mixer.clipAction( result.clip ).name=name	
 					trunk.actions[name]=trunk.mixer.clipAction( result.clip )
+					if(name==trunk.defaultAction){
+						trunk.mixer.clipAction( result.clip ).setEffectiveWeight(1).play()
+						trunk.doing=trunk.defaultAction
+					}
 				} );
 			}
 		)
@@ -181,22 +201,66 @@ var Character=( function(){
 		trunk.todo=function(name,keep){
 			var alter=this
 			alter.actions[alter.doing].setEffectiveWeight(0).play()
-			if(name!='sway'){alter.actions[name].setEffectiveWeight(0).reset()}
+			// if(name!=alter.defaultAction && alter.stamina<=0){
+			// 	return
+			// }
+			alter.actions[name].setEffectiveWeight(0).reset()
 			alter.actions[name].setEffectiveWeight(1).play()
 			alter.doing=name
 			// alter.hideMarks()
+			// alter.stamina-=1
+			// console.log(alter.stamina)
+			// console.log(alter.position)
 			
 			if(keep && keep!=0){
 				alter.control=true
 				setTimeout(function(){
 					// alter.showMarks()
 					alter.control=false
+					alter.todo(alter.defaultAction)
 				},keep*1000)
 			}
 
 			setTimeout(function(){
 				alter.showMarks()
 			},keep*1100)
+		}
+		//  cast skills ---------------------
+
+		trunk.cast=function(skillname){
+			var alter=this
+			var relative
+			var absolute=[]
+			data.setting.forEach(function(parameter){
+				if(career==parameter.career){
+					parameter.skills.forEach(function(ss){
+						if(ss.name==skillname){
+							relative=ss.operator
+							// console.log(ss.operator[0].position)
+						}
+						
+					})
+				}
+			})
+			alter.position.d=alter.position.d%4
+			if(alter.position.d<0){alter.position.d+=4}
+			relative.forEach(function(oper){
+				// console.log(oper)
+				var cos=1,sin=0
+				if(alter.position.d==1){cos= 0,sin= 1}
+				if(alter.position.d==2){cos=-1,sin= 0}
+				if(alter.position.d==3){cos= 0,sin=-1}
+				var oper1={position:{x: 1,y:0,z: 1,d:0}}
+				oper1.position.x=oper.position.z*sin+oper.position.x*cos+alter.position.x
+				oper1.position.y=oper.position.y+alter.position.y
+				oper1.position.z=oper.position.z*cos-oper.position.x*sin+alter.position.z
+				oper1.position.d=oper.position.d+alter.position.d
+				// console.log(oper.position)
+				// console.log(oper1.position)
+				absolute.push(oper1)
+			})
+			// console.log(absolute)
+			return absolute		
 		}
 
 		// effects ---------------------------
@@ -290,21 +354,22 @@ var Character=( function(){
 		}	
 
 		// character commands-------------------------
-		trunk.doSingleCommand=function(command,delay){
+		trunk.doSingleCommand=function(command,callback){
 			var alter=this
+			var operators
 			if(alter.control){return}
-			if(!delay){delay=0}
-			setTimeout(function(){
+			// if(!delay){delay=0}
+			// setTimeout(function(){
 				if(command=='w' ){               
 					alter.todo('walk',1)
 					alter.move('forward',1)
 				}
 				if(command=='s'){
 					alter.todo('salute',1)
-					alter.puts('sword-right')
-					alter.take('sword-left')
-					alter.take('sword-right',1)
-					alter.puts('sword-left',1)
+					// alter.puts('sword-right')
+					// alter.take('sword-left')
+					// alter.take('sword-right',1)
+					// alter.puts('sword-left',1)
 				}
 				if(command=='d'){
 					alter.todo('trig',1)
@@ -318,29 +383,25 @@ var Character=( function(){
 					alter.todo('hack',1)
 					alter.show('hack',.3)
 					alter.hide('hack',1)
+					operators=alter.cast('hack') // !!!!!!!
 				}
 				if(command=='c'){
 					alter.todo('swep',1)
 					alter.show('swep',.3)
 					alter.hide('swep',1)
+					operators=alter.cast('swep') // !!!!!!!
 				}
 				if(command=='z'){
 					alter.todo('spur',1)
 					alter.show('spur',.3)
 					alter.hide('spur',1)
+					operators=alter.cast('spur') // !!!!!!!
 				}
-			},delay*1000)
+			// },delay*1000)
+			callback(operators)
+			return operators
 		}
 		// ------------------------------
-		/* [ index.html ]
-			function onKeyPress(event){
-				peoples.forEach(function(element) {
-					if(element.name!="a"){                        
-						element.doSingleCommand(event.key)
-					}
-				});
-			}
-		*/
 		return trunk
 	}
 	return Character;
